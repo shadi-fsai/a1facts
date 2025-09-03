1eb5fe2f8252539b245f0e5c9ddab4ef266bee68
shadi-fsai <163780314+shadi-fsai@users.noreply.github.com>
2025-09-02 20:45:21 -0700

    move to posthog for telemetry

--- src/utils/telemetry.py
+++ src/utils/telemetry.py
@@ -1,28 +1,30 @@
-import requests
 import os
 import threading
+
+from posthog import Posthog
+
+posthog = Posthog(
+  project_api_key='phc_DPuhaEbPkRMYteIVSLoi0b4ZVqZ3afLq8M6klC0QCX9',
+  host='https://us.i.posthog.com'
+)
 
 # It's crucial to allow users to opt-out.
 # An environment variable is a standard way to do this.
 TELEMETRY_ENABLED = os.environ.get("A1FACTS_TELEMETRY_DISABLED") != "1"
 
-# Your unique URL from Pipedream or another webhook service.
-TELEMETRY_URL = "https://eon59vvwyxpjj4c.m.pipedream.net" 
 
-def send_telemetry_ping():
+
+def send_telemetry_ping(event_string: str, properties: dict={}):
     """
     Sends a non-blocking request to the telemetry endpoint.
     """
     if TELEMETRY_ENABLED:
         try:
-            # The timeout prevents the request from hanging indefinitely.
-            requests.post(TELEMETRY_URL, timeout=2) 
-            # You don't need to send any data; Pipedream logs the IP from the request itself.
-        except requests.RequestException:
+            posthog.capture(event_string, properties=properties)
+        except Exception:
             # Fail silently if there's a network error or the service is down.
             pass
 
-def nonblocking_send_telemetry_ping():
-    telemetry_thread = threading.Thread(target=send_telemetry_ping)
+def nonblocking_send_telemetry_ping(event_string: str="a1facts_run", properties: dict={}):
+    telemetry_thread = threading.Thread(target=send_telemetry_ping, args=(event_string, properties))
     telemetry_thread.start()
-
